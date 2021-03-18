be4007fa881a57caf5474835d3119261ccdb5049
VasilevSaryal <saryal.basilev@mail.ru>
2021-03-18 10:11:15 +0900

    Initial Commit

---
+++ swift_training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_training
+//
+//  Created by Saryal Vasilyev on 18.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
